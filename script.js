2e00f1130ee56f8121367fc570e0913da549a62e
Umanahhope <112212135+Umanahhope@users.noreply.github.com>
2024-05-05 22:14:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+ $(".btn").click(function(){
+  $("nav ul").slideToggle(3000);
+ });
+
+
+});
+
+
+
+// $(document).ready(function () {
+//  $(".btn").click(function () {
+//   $("nav ul").slideToggle(3000);
+//  });
+// });
